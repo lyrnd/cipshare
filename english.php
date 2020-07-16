0ce4edaa188b91d703be3adda2b3a08f71bf28d6
GrantWallich <67800413+GrantWallich@users.noreply.github.com>
2020-07-16 11:26:46 -0600

    Add files via upload

---
+++ english.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'trans' => 
+  array (
+    'Main Interface' => 
+    array (
+      'My Files' => 'CIPShare Files',
+      'Contents' => 'Advanced Search',
+      'Date Format: Files' => 'm/d/Y h:i A',
+      'Name' => 'Simple Search',
+    ),
+    'General' => 
+    array (
+      'Submit' => 'Submit',
+      'Save' => 'Save',
+      'Cancel' => 'Cancel',
+      'Error' => 'Error',
+      'Invalid path.' => 'Invalid path.',
+      'File not found!' => 'File not found!',
+      'You are not allowed to preview the selected file.' => 'You are not allowed to preview the selected file.',
+      '%1 users' => '%1 users',
+    ),
+    'Admin: Users' => 
+    array (
+      'Date Format: Registration date' => 'd M Y',
+      'Date Format: Last login' => 'd M Y, H:i',
+      'Date Format: Expiration date' => 'd M Y',
+    ),
+    'Admin: Logs' => 
+    array (
+      'Date Format: Grid - Date' => 'm/d/Y h:i A',
+    ),
+  ),
+);
